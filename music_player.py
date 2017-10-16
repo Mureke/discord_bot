f031fdcf8a8447ba44c7223e76851a85904a43a4
Markus Suomalainen <markus.suomalainen@gmail.com>
2017-10-17 00:26:48 +0300

    small changes to music player

--- music_player.py
+++ music_player.py
@@ -3,18 +3,18 @@ import asyncio
 
 class Player:
     def __init__(self, voice):
-        self.voice = voice
         self.player_instance = None
         self.command = ''
+        self._voice = voice
 
     @asyncio.coroutine
     def play(self):
         """
         Plays song from file
         """
-        self.player_check()
+        self._player_check()
         song = self.command.replace('!player play ', '')
-        self.player_instance = self.voice.create_ffmpeg_player('music/%s.mp3' % song)
+        self.player_instance = self._voice.create_ffmpeg_player('music/%s.mp3' % song)
         self.player_instance.volume = 0.25
         self.player_instance.start()
 
@@ -23,14 +23,14 @@ class Player:
         """
         Plays song with youtube-dl
         """
-        self.player_check()
-        if 'youtube.com' in self.command:
+        self._player_check()
+        if 'youtu' in self.command:
             yt_link = self.command.replace('!player ytplay ', '')
         else:
             yt_link = 'https://www.youtube.com/watch?v=%s' % self.command.replace('!player ytplay ', '')
 
         try:
-            self.player_instance = yield from self.voice.create_ytdl_player(yt_link)
+            self.player_instance = yield from self._voice.create_ytdl_player(yt_link)
             self.player_instance.volume = 0.25
             self.player_instance.start()
         except Exception as e:
@@ -48,7 +48,8 @@ class Player:
     def resume(self):
         self.player_instance.resume()
 
-    # TODO: Do this somewhere else... or add queue... ;)
-    def player_check(self):
+    # TODO: Do this somewhere else... or add queue feature... ;)
+    def _player_check(self):
+        print(self._voice)
         if self.player_instance is not None and self.player_instance.is_playing():
             self.player_instance.stop()
